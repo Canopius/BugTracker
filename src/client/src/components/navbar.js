67fbf5520ee059e5570535620a412ad092d26eb7
Canopius <canopius@DESKTOP-KJC5QOG.localdomain>
2021-12-15 19:58:50 +0000

    Start of nav bar

--- src/client/src/components/navbar.js
+++ src/client/src/components/navbar.js
@@ -11,22 +11,6 @@ const Navbar = () =>{
 		<div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
 
-
-        <NavLink className="navbar-brand" to="/">
-          e!
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
